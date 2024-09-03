08332b5775c096e162491169ee5a43e517cde50e
MADHAN79 <madhanvarshini3@gmail.com>
2024-09-03 17:09:03 +0530

    port updated

--- config/db.js
+++ config/db.js
@@ -3,14 +3,12 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Connect to MongoDB using the connection string from the environment variables
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error(error.message);
-    process.exit(1);
+    process.exit(1); // Exit the process with a failure code
   }
 };
 
