eca4ccae62f604664017ddef4285a729c751f30a
MADHAN79 <madhanvarshini3@gmail.com>
2024-09-03 17:30:25 +0530

    port updated

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.use('/api/customers', customerRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/rooms', roomRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
